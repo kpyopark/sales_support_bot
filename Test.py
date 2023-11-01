d1b5f91d5f615796b326f6f093f767efabf47a84
Kyungpyo Park <toheavener01@gmail.com>
2023-11-01 12:15:14 +0900

    Initial Commit

---
+++ Test.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import openai
+
+st.write("Hello world!")
+
+
+with st.sidebar:
+  st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+
+st.title("💬 Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not st.session_state.chatbot_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = st.session_state.chatbot_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
+    
